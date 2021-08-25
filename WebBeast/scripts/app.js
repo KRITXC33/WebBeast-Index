1816cc0e5b64e66d92dbd9543fdc6611fc2aeff6
king <79935756+kingdec@users.noreply.github.com>
2021-08-25 15:05:19 -0700

    scripts

---
+++ WebBeast/scripts/app.js
@@ -0,0 +1,10 @@
+var app = (function () {
+    /* Properties */
+    var websiteName = "kdccodes"
+    /*Methods*/
+    return {
+        getWebsiteName: function () {
+            return websiteName;
+        }
+    }
+} ();
